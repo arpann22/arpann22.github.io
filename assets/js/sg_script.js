9679a1b87d14d526537a8bc38f08a372418e5c93
Code Pixelz Media <arpanadhikari63@gmail.com>
2024-07-30 10:21:09 +0545

    responsive changes and login/register page

--- assets/js/sg_script.js
+++ assets/js/sg_script.js
@@ -9,19 +9,27 @@ jQuery(document).ready(function ($) {
         slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                     infinite: true,
-                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
@@ -33,4 +41,31 @@ jQuery(document).ready(function ($) {
             }
         ]
     });
+
+
+    // hide stats for mobile and desktop alternatively
+    function applyResponsiveClasses() {
+        const width = $(window).width();
+        if (width > 1024) {
+            $('.hide-desktop').hide();
+            $('.hide-tabs').show();
+            $('.hide-mobile').show();
+        } else if (width > 767 && width <= 1024) {
+            $('.hide-desktop').show();
+            $('.hide-tabs').hide();
+            $('.hide-mobile').show();
+        } else {
+            $('.hide-desktop').show();
+            $('.hide-tabs').show();
+            $('.hide-mobile').hide();
+        }
+    }
+
+    $(document).ready(function () {
+        applyResponsiveClasses();
+        $(window).resize(function () {
+            applyResponsiveClasses();
+        });
+    });
+
 });
